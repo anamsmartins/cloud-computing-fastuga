b91e5c69423a0a6f814a070c048d0f339f7197b2
anamsmartins <anaritamsmartins@gmail.com>
2024-04-06 10:45:32 +0100

    final touches

--- fastuga-laravel/entrypoint.sh
+++ fastuga-laravel/entrypoint.sh
@@ -3,11 +3,18 @@
 # Start the Laravel server
 php artisan serve --host=0.0.0.0 --port=8081 &
 
-# Run Laravel migrations, seeds, etc.
-php artisan migrate
-php artisan db:seed
-php artisan storage:link
-php artisan passport:install
+if [ ! -f /var/www/html/migration_completed.txt ]; then
+    # Run Laravel migrations, seeds, etc.
+    php artisan migrate
+    php artisan db:seed
+    php artisan storage:link
+    php artisan passport:install
+
+    # Create a file to indicate migration completion
+    touch /var/www/html/migration_completed.txt
+else
+    echo "Migrations have already been performed, skipping..."
+fi
 
 # Keep the container running
 tail -f /dev/null
