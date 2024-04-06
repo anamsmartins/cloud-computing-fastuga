7be61ab0aa9f5f3eb1541f5995fe70893254eecf
anamsmartins <anaritamsmartins@gmail.com>
2024-04-06 23:05:10 +0100

    Database Cloud Deployment

--- Makefile
+++ Makefile
@@ -1,8 +1,4 @@
 up:
 	docker-compose up -d
 down:
-	docker exec fastuga-api bash -c "php artisan cache:clear"
-	docker exec fastuga-api bash -c "php artisan config:clear"
-	docker exec fastuga-api bash -c "composer dump-autoload"
-	docker exec fastuga-api bash -c "sudo chmod -R 755 storage"
 	docker-compose down -v --rmi "local"
--- fastuga-laravel/entrypoint.sh
+++ fastuga-laravel/entrypoint.sh
@@ -3,6 +3,14 @@
 # Start the Laravel server
 php artisan serve --host=0.0.0.0 --port=8081 &
 
+/var/www/html/cloud_sql_proxy -instances=cloud-computing-prj1-fastuga:us-central1:fastuga-database=tcp:3306 &
+
+# Wait for Cloud SQL Proxy to start
+while ! mysqladmin ping -h 127.0.0.1 --silent; do
+    echo "Waiting for Cloud SQL Proxy to start..."
+    sleep 1
+done
+
 if [ ! -f /var/www/html/migration_completed.txt ]; then
     # Run Laravel migrations, seeds, etc.
     php artisan migrate
