e1f382f717153313eca82a44f89c06ddfcc2de79
anamsmartins <anaritamsmartins@gmail.com>
2024-04-07 21:25:35 +0100

    possible fix on how to access secrets

--- config/database.php
+++ config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL', ''),
+            'url' => getenv('DATABASE_URL'),
             'host' => env('DB_HOST', 'fastuga-database-cloud-computing-fastuga.a.aivencloud.com'),
             'port' => env('DB_PORT', '17883'),
             'database' => env('DB_DATABASE', 'defaultdb'),
             'username' => env('DB_USERNAME', 'avnadmin'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => getenv('DB_PASSWORD'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
