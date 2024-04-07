fa5c35fe918fd4886e2b717da3cf5da716933218
anamsmartins <anaritamsmartins@gmail.com>
2024-04-07 23:53:12 +0100

    another attempt

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '17883'),
             'database' => env('DB_DATABASE', 'defaultdb'),
             'username' => env('DB_USERNAME', 'avnadmin'),
-            'password' => env('DB_PASSWORD', ${DB_PASSWORD}),
+            'password' => getenv('DB_PASSWORD'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
